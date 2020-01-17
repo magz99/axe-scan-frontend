e1355075e414c0882ff01c149c31f50dc381abf4
magz99 <magzb.99@gmail.com>
2020-01-16 22:08:07 -0500

    feat: adding scan routes setup

---
+++ json-server/routes/scans.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+// TODO: This should come from the AWS api and cached in our Store.
+router.get('/', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res
+    .status(200)
+    .sendFile('site-list.json', { root: path.join(__dirname, '../public') });
+});
+
+// TODO: This should come from the AWS api and cached in our Store.
+router.get('/:sitename/:foldername/:scanfile', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).sendFile('rangle-scan-list.json', {
+    root: path.join(__dirname, '../public'),
+  });
+});
+
+module.exports = router;
