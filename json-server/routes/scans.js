b9ade3e0a37e6d0e41723f7e38e0a39526ecb94a
magz99 <magzb.99@gmail.com>
2020-01-19 19:30:22 -0500

    Feat: Updating routes and adding new scans Routes.

--- json-server/routes/scans.js
+++ json-server/routes/scans.js
@@ -5,16 +5,26 @@ var router = express.Router();
 // TODO: This should come from the AWS api and cached in our Store.
 router.get('/', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res
-    .status(200)
-    .sendFile('site-list.json', { root: path.join(__dirname, '../public') });
+  res.status(200).sendFile('www.toyota.ca_1.json', {
+    root: path.join(__dirname, '../public/sample-scans'),
+  });
 });
-
-// TODO: This should come from the AWS api and cached in our Store.
-router.get('/:sitename/:foldername/:scanfile', function(req, res, next) {
+router.get('/:siteName/:folderName', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).sendFile('rangle-scan-list.json', {
-    root: path.join(__dirname, '../public'),
+  res.status(200).sendFile('master.json', {
+    root: path.join(
+      __dirname,
+      `../public/sample-scans/${req.params.siteName}/${req.params.folderName}`
+    ),
+  });
+});
+router.get('/:siteName/:folderName/:scanFile', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).sendFile(`${req.params.scanFile}`, {
+    root: path.join(
+      __dirname,
+      `../public/sample-scans/${req.params.siteName}/${req.params.folderName}`
+    ),
   });
 });
 
--- json-server/routes/sites.js
+++ json-server/routes/sites.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var router = express.Router();
 
 // TODO: This should come from the AWS api and cached in our Store.
@@ -11,11 +12,21 @@ router.get('/', function(req, res, next) {
 });
 
 // TODO: This should come from the AWS api and cached in our Store.
-router.get('/rangle.io', function(req, res, next) {
+router.get('/:siteName', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).sendFile('rangle-scan-list.json', {
-    root: path.join(__dirname, '../public'),
-  });
+
+  // var fPath = path.join(__dirname, '../public/rangle-scan-list.json');
+  // var file = fs.readFileSync(fPath, 'utf-8');
+  // var jsonFile = JSON.parse(file);
+  if (req.params.siteName === 'rangle.io') {
+    res.status(200).sendFile('rangle-scan-list.json', {
+      root: path.join(__dirname, '../public'),
+    });
+  }
+  // var jsonRes = {};
+  // jsonRes.scans = jsonFile.CommonPrefixes;
+  // res.status(200);
+  // res.json(jsonRes);
 });
 
 module.exports = router;
