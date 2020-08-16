072b11b3cf351c1ecc2e09ca132ce8cb43fee1c1
magz99 <magzb.99@gmail.com>
2020-08-16 12:36:47 -0400

    Updates backend to load all from AWS, model updates, and frontend data displaying

--- json-server/routes/scans.js
+++ json-server/routes/scans.js
@@ -1,31 +1,70 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var AWS = require('aws-sdk');
+const { AWS_SCAN_BUCKET } = require('./shared/common-vars');
+const s3 = new AWS.S3();
 
-// TODO: This should come from the AWS api and cached in our Store.
-router.get('/', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).sendFile('www.toyota.ca_1.json', {
-    root: path.join(__dirname, '../public/sample-scans'),
+// Pull the "master json file" by default because this will contain
+// A summary of the scans, as well as a mapping of urls -> result json
+router.get('/:siteName/:folderName', function (req, res, next) {
+  const params = {
+    Bucket: AWS_SCAN_BUCKET,
+    Key: `${req.params.siteName}/${req.params.folderName}/scan-results.json`,
+  };
+
+  s3.getObject(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.status(500);
+    } else {
+      if (data) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.status(200);
+
+        const bodyValueAsString = data.Body.toString(
+          'utf-8',
+          0,
+          data.Body.length
+        );
+        res.json(JSON.parse(bodyValueAsString));
+      }
+    }
   });
 });
-router.get('/:siteName/:folderName', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).sendFile('master.json', {
-    root: path.join(
-      __dirname,
-      `../public/sample-scans/${req.params.siteName}/${req.params.folderName}`
-    ),
+
+router.get('/:siteName/:folderName/:scanFile', function (req, res, next) {
+  const params = {
+    Bucket: AWS_SCAN_BUCKET,
+    Key: `${req.params.siteName}/${req.params.folderName}/${req.params.scanFile}`,
+  };
+
+  s3.getObject(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.status(500);
+    } else {
+      if (data) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.status(200);
+
+        const bodyValueAsString = data.Body.toString(
+          'utf-8',
+          0,
+          data.Body.length
+        );
+        res.json(JSON.parse(bodyValueAsString));
+      }
+    }
   });
-});
-router.get('/:siteName/:folderName/:scanFile', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).sendFile(`${req.params.scanFile}`, {
-    root: path.join(
-      __dirname,
-      `../public/sample-scans/${req.params.siteName}/${req.params.folderName}`
-    ),
-  });
+
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.status(200).sendFile(`${req.params.scanFile}`, {
+  //   root: path.join(
+  //     __dirname,
+  //     `../public/sample-scans/${req.params.siteName}/${req.params.folderName}`
+  //   ),
+  // });
 });
 
 module.exports = router;
---
+++ json-server/routes/shared/common-vars.js
@@ -0,0 +1,3 @@
+var AWS_SCAN_BUCKET = 'project-axe-tool-test-bucket-json-results';
+
+exports.AWS_SCAN_BUCKET = AWS_SCAN_BUCKET;
