1d6cf06e70da72565e1595ab1040f20e8f378193
Magali Bautista-Lee <magzb.99@gmail.com>
2020-09-07 19:24:37 -0400

    Creating proxy file, updating http requests (#28)
    * Creating proxy file, updating http requests
    
    * removing proxy file, adding gitignore

--- src/app/services/scan-loader.service.ts
+++ src/app/services/scan-loader.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const SITE_MASTER_SCAN_URL =
-  'https://9fqgrybhg2.execute-api.us-east-1.amazonaws.com/scannedSingleSiteMappingS3';
-
-const SITE_SINGLES_SCAN_URL =
-  'https://hlkgqouo08.execute-api.us-east-1.amazonaws.com/singleSiteSingleScan';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +10,7 @@ export class ScanLoaderService {
 
   getMasterJSON(sitename: string, foldername: string) {
     return this.http.get(
-      `${SITE_MASTER_SCAN_URL}?siteName=${sitename}&folderName=${foldername.replace(
+      `api/scannedSingleSiteMappingS3?siteName=${sitename}&folderName=${foldername.replace(
         /:/gi,
         ''
       )}`
@@ -28,7 +23,7 @@ export class ScanLoaderService {
     filename: string
   ): Observable<any> {
     return this.http.get(
-      `${SITE_SINGLES_SCAN_URL}?siteName=${sitename}&folderName=${foldername.replace(
+      `api/singleSiteSingleScan?siteName=${sitename}&folderName=${foldername.replace(
         /:/gi,
         ''
       )}&scanFile=${filename}`
