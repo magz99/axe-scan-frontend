bb386bebca3b28966fcd2765277fc84b1c305945
Magali Bautista-Lee <magzb.99@gmail.com>
2020-09-07 18:23:44 -0400

    Updating API urls, removing json server. Correcting some routes (#27)

--- src/app/services/site-listing.service.ts
+++ src/app/services/site-listing.service.ts
@@ -2,7 +2,12 @@ import { ApiUrls } from './site-urls';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const SITE_LIST_URL = 'http://localhost:3000/';
+const SITE_LIST_URL =
+  'https://0huw2vaqr3.execute-api.us-east-1.amazonaws.com/default/scannedSiteListS3';
+
+const SITE_SCAN_LIST_URL =
+  'https://xuuxj86tmb.execute-api.us-east-1.amazonaws.com/default/singleSiteAllScans';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,11 +16,11 @@ export class SiteListingService {
 
   // Returns a list of websites for which we have scans
   getSiteList() {
-    return this.http.get(SITE_LIST_URL + ApiUrls.SITES);
+    return this.http.get(SITE_LIST_URL);
   }
 
   // Return the timestamped folders for a particular site
   getSiteScans(siteName: string) {
-    return this.http.get(SITE_LIST_URL + ApiUrls.SITES + '/' + siteName);
+    return this.http.get(`${SITE_SCAN_LIST_URL}?siteName=${siteName}`);
   }
 }
