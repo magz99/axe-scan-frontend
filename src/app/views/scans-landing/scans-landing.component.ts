35f1fd4082add7b8d8490284d0cd32f1fc7202ba
magz99 <magzb.99@gmail.com>
2021-01-03 12:20:17 -0500

    Cleaning up component

--- src/app/views/scans-landing/scans-landing.component.ts
+++ src/app/views/scans-landing/scans-landing.component.ts
@@ -1,5 +1,5 @@
 import { AWSListing, AWSSite } from '../../services/types/sites.types';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SiteListingFacadeService } from '../../services/site-listing-facade.service';
 
@@ -8,24 +8,11 @@ import { SiteListingFacadeService } from '../../services/site-listing-facade.ser
   templateUrl: './scans-landing.component.html',
   styleUrls: ['./scans-landing.component.less'],
 })
-export class ScansLandingComponent implements OnInit, OnDestroy {
+export class ScansLandingComponent {
   siteList$: Observable<AWSListing>;
   siteList: AWSSite[];
 
   constructor(private siteListingFacade: SiteListingFacadeService) {
     this.siteList$ = this.siteListingFacade.getSites();
   }
-
-  // Use ngOnInit for:
-  // To perform complex initializations shortly after construction
-  // To set up the component after Angular sets the input properties.
-  ngOnInit() {
-  }
-
-  // Put cleanup logic in ngOnDestroy(), the logic that must run before Angular destroys the directive.
-  // This is the place to free resources that won't be garbage collected automatically.
-  // Unsubscribe from Observables and DOM events. Stop interval timers. Unregister all callbacks that
-  //  this directive registered with global or application services. You risk memory leaks if you neglect to do so.
-  // This is the time to notify another part of the application that the component is going away.
-  ngOnDestroy() {}
 }
